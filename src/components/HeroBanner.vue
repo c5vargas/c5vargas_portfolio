485d6acba1f26bc5d1dabe90f88a97063afd5010
Carles Vargas <carlesvargas5@gmail.com>
2025-08-12 13:58:40 +0200

    fix: removed prettier/prettier from eslint config

---
+++ src/components/HeroBanner.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="hero-banner">
+    <h1 class="hero-title"></h1>
+    <p class="hero-subtitle"></p>
+    <button class="hero-button" @click="handleClick">TRABAJEMOS JUNTOS</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const handleClick = () => {
+  console.log('Button clicked!')
+}
+</script>
