c149db7a9abc7a31a0756daab2b783da0574f94e
Carles Vargas <carlesvargas5@gmail.com>
2025-08-19 09:49:48 +0200

    feat(icons): added software collection icons

---
+++ src/components/icons/software/index.ts
@@ -0,0 +1,21 @@
+import CapacitorIcon from './CapacitorIcon.vue'
+import CloudflareIcon from './CloudflareIcon.vue'
+import HTMLIcon from './HTMLIcon.vue'
+import JavascriptIcon from './JavascriptIcon.vue'
+import LaravelIcon from './LaravelIcon.vue'
+import PostmanIcon from './PostmanIcon.vue'
+import TypescriptIcon from './TypescriptIcon.vue'
+import VuejsIcon from './VuejsIcon.vue'
+import ReactIcon from './ReactIcon.vue'
+
+export {
+  CapacitorIcon,
+  CloudflareIcon,
+  HTMLIcon,
+  JavascriptIcon,
+  LaravelIcon,
+  PostmanIcon,
+  ReactIcon,
+  TypescriptIcon,
+  VuejsIcon,
+}
