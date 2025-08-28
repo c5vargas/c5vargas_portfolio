db2c39a255204ecbc4da33f8189e3e6a436ff834
Carles Vargas <carlesvargas5@gmail.com>
2025-08-28 11:15:11 +0200

    feat(router): instantly scroll to top on route change

--- src/router.ts
+++ src/router.ts
@@ -11,6 +11,6 @@ export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior() {
-    return { top: 0 }
+    return { top: 0, behavior: 'instant' }
   },
 })
